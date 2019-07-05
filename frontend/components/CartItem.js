cf79aeb30811e6fa7cd5e960204bc2fb79d04bf7
Rafael Companhoni <companhoni@gmail.com>
2019-07-05 02:19:49 -0300

    Fix 'Remove Item' feature

--- frontend/components/CartItem.js
+++ frontend/components/CartItem.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 import formatMoney from "../lib/formatMoney";
-import RemoveFromCart from './RemoveFromCart';
+import RemoveFromCart from "./RemoveFromCart";
 
 const CartItemStyles = styled.li`
   padding: 1rem 0;
@@ -17,24 +17,34 @@ const CartItemStyles = styled.li`
   }
 `;
 
-const CartItem = ({ cartItem }) => (
-  <CartItemStyles>
-    <img width="100" src={cartItem.item.image} alt={cartItem.item.title} />
+const CartItem = ({ cartItem }) => {
+  if (!cartItem.item) {
+    return (
+      <CartItemStyles>
+        <p>This item has been removed</p>
+        <RemoveFromCart id={cartItem.id} />
+      </CartItemStyles>);
+  }
 
-    <div className="cart-item-details">
-      <h3>{cartItem.item.title}</h3>
-      <p>
-        {formatMoney(cartItem.item.price * cartItem.quantity)}
-        {' - '}
-        <em>
-          {cartItem.quantity} &times; {formatMoney(cartItem.item.price)} each
-        </em>
-      </p>
-    </div>
+  return (
+    <CartItemStyles>
+      <img width="100" src={cartItem.item.image} alt={cartItem.item.title} />
 
-    <RemoveFromCart id={cartItem.id} />
-  </CartItemStyles>
-);
+      <div className="cart-item-details">
+        <h3>{cartItem.item.title}</h3>
+        <p>
+          {formatMoney(cartItem.item.price * cartItem.quantity)}
+          {" - "}
+          <em>
+            {cartItem.quantity} &times; {formatMoney(cartItem.item.price)} each
+          </em>
+        </p>
+      </div>
+
+      <RemoveFromCart id={cartItem.id} />
+    </CartItemStyles>
+  );
+};
 
 CartItem.propTypes = {
   cartItem: PropTypes.object.isRequired
