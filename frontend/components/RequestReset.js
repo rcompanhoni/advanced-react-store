57b8f17159cd787f296b5f8adfa2d9d5ce807aaf
Rafael Companhoni <companhoni@gmail.com>
2019-07-11 11:23:45 -0300

    RequestReset (example of Enzyme 'simulate' for typing in an input)

--- frontend/components/RequestReset.js
+++ frontend/components/RequestReset.js
@@ -5,7 +5,7 @@ import Form from './styles/Form';
 import Error from './ErrorMessage';
 
 const REQUEST_RESET_MUTATION = gql`
-  mutation SIGNIN_MUTATION($email: String!) {
+  mutation REQUEST_RESET_MUTATION($email: String!) {
     requestReset(email: $email) {
       message
     }
@@ -14,55 +14,47 @@ const REQUEST_RESET_MUTATION = gql`
 
 class RequestReset extends Component {
   state = {
-    email: ''
-  }
-
-  saveToState = (e) => {
-    this.setState({ [e.target.name]: e.target.value })
-  }
-
+    email: '',
+  };
+  saveToState = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
   render() {
     return (
-      <Mutation
-        mutation={REQUEST_RESET_MUTATION}
-        variables={this.state}
-      >
-      {
-        (requestReset, { error, loading, called }) => {
-          return (
-            <Form method="post" onSubmit={(e) => {
+      <Mutation mutation={REQUEST_RESET_MUTATION} variables={this.state}>
+        {(reset, { error, loading, called }) => (
+          <Form
+            method="post"
+            data-test="form"
+            onSubmit={async e => {
               e.preventDefault();
-              requestReset();
-            }}>
-              <fieldset disabled={loading} aria-busy={loading}>
-                <h2>Request a password reset</h2>
+              await reset();
+              this.setState({ email: '' });
+            }}
+          >
+            <fieldset disabled={loading} aria-busy={loading}>
+              <h2>Request a password reset</h2>
+              <Error error={error} />
+              {!error && !loading && called && <p>Success! Check your email for a reset link!</p>}
+              <label htmlFor="email">
+                Email
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="email"
+                  value={this.state.email}
+                  onChange={this.saveToState}
+                />
+              </label>
 
-                <Error error={error} />
-
-                {!error && !loading && called && 
-                  <p>Success! Check your email for a reset link</p>
-                }
-
-                <label htmlFor="email">
-                  Email
-                  <input
-                    type="email"
-                    name="email"
-                    placeholder="email"
-                    value={this.state.email}
-                    onChange={this.saveToState}
-                  />
-                </label>
-
-                <button type="submit">Request Reset!</button>
-              </fieldset>
-            </Form>
-          )
-        }
-      }
+              <button type="submit">Request Reset!</button>
+            </fieldset>
+          </Form>
+        )}
       </Mutation>
-    )
+    );
   }
 }
 
 export default RequestReset;
+export { REQUEST_RESET_MUTATION };
