96f8748d95a3073f942c62895e8a77e96eb39f35
Rafael Companhoni <companhoni@gmail.com>
2019-07-11 12:29:38 -0300

    Order test

--- frontend/components/Order.js
+++ frontend/components/Order.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react'
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Query } from 'react-apollo';
-import gql from "graphql-tag";
 import { format } from 'date-fns';
 import Head from 'next/head';
+import gql from 'graphql-tag';
 import formatMoney from '../lib/formatMoney';
+import Error from './ErrorMessage';
 import OrderStyles from './styles/OrderStyles';
 
 const SINGLE_ORDER_QUERY = gql`
@@ -29,11 +30,10 @@ const SINGLE_ORDER_QUERY = gql`
   }
 `;
 
-class Order extends Component {
+class Order extends React.Component {
   static propTypes = {
-    id: PropTypes.string.isRequired
-  }
-
+    id: PropTypes.string.isRequired,
+  };
   render() {
     return (
       <Query query={SINGLE_ORDER_QUERY} variables={{ id: this.props.id }}>
@@ -41,41 +41,34 @@ class Order extends Component {
           if (error) return <Error error={error} />;
           if (loading) return <p>Loading...</p>;
           const order = data.order;
-          
           return (
-            <OrderStyles>
+            <OrderStyles data-test="order">
               <Head>
                 <title>Sick Fits - Order {order.id}</title>
               </Head>
-
               <p>
                 <span>Order ID:</span>
                 <span>{this.props.id}</span>
               </p>
-
               <p>
-                <span>Charge:</span>
+                <span>Charge</span>
                 <span>{order.charge}</span>
               </p>
-
               <p>
-                <span>Date:</span>
-                <span>{format(order.createdAt, 'MMMM d, YYYY h:mm: a')}</span>
+                <span>Date</span>
+                <span>{format(order.createdAt, 'MMMM d, YYYY h:mm a')}</span>
               </p>
-
               <p>
-                <span>Total:</span>
+                <span>Order Total</span>
                 <span>{formatMoney(order.total)}</span>
               </p>
-
               <p>
-                <span>Item Count:</span>
+                <span>Item Count</span>
                 <span>{order.items.length}</span>
               </p>
-
               <div className="items">
                 {order.items.map(item => (
-                  <div key={item.id} className="order-item">
+                  <div className="order-item" key={item.id}>
                     <img src={item.image} alt={item.title} />
                     <div className="item-details">
                       <h2>{item.title}</h2>
@@ -91,8 +84,9 @@ class Order extends Component {
           );
         }}
       </Query>
-    )
+    );
   }
 }
 
 export default Order;
+export { SINGLE_ORDER_QUERY };
